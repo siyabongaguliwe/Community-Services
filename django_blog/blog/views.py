04cca033f06cb1be7ebe0c8940011173e5cd7951
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2025-10-28 22:11:41 +0200

    Finalize capstone backend: cleaned serializers/views, passed permission tests, updated README and Loom demo prep

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Listing, Service, Event, Review
-from .serializers import ListingSerializer, ServiceSerializer, EventSerializer, ReviewSerializer
-from rest_framework import generics
+from .serializers import (
+    ListingSerializer,
+    ServiceSerializer,
+    EventSerializer,
+    ReviewSerializer,
+)
 from .permissions import IsOwnerOrAdmin
-from .serializers import BusinessProfileSerializer
-from .models import BusinessProfile
 
+# --- ViewSets for standard CRUD operations ---
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+    permission_classes = [IsOwnerOrAdmin]
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [IsOwnerOrAdmin]
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    permission_classes = [IsOwnerOrAdmin]
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
-# Create your views here.
-# views.py
-
-class BusinessProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = BusinessProfile.objects.all()
-    serializer_class = BusinessProfileSerializer
     permission_classes = [IsOwnerOrAdmin]
+
+# --- Detail views with permission control ---
 
 
 class ListingDetailView(generics.RetrieveUpdateDestroyAPIView):
